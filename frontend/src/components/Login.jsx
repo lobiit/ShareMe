0cc499257d02c27cf4395764be7511583a7c9cf5
Tumbo Lobi John <johnlobiit@gmail.com>
2023-03-27 11:30:25 +0300

    created the login component and added its features

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,27 @@
 import React from  'react';
 import {useNavigate} from "react-router-dom";
-
-function Login(){
+import shareVideo from '../assets/share.mp4';
+import  logo from '../assets/logo.png';
+const Login = ()=>{
     return(
-        <div>
-            Login
+        <div className="flex justify-start items-center flex-col h-screen">
+            <div className="relative w-full h-full">
+                <video
+                src={shareVideo}
+                type="video/mp4"
+                loop
+                controls={false}
+                muted
+                autoPlay
+                className="w-full h-full object-cover"
+                />
+                <div className="absolute flex flex-col justify-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+                    <div className="p-5">
+                        <img src={logo} width="130px" alt="logo"/>
+                    </div>
+                </div>
+            </div>
         </div>
-    );
+    )
 }
 export  default Login
