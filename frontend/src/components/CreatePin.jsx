1dc4a7af1364405bbaea5023f6c189ddb5d5139b
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-04 18:34:45 +0300

    save pin functionality bug fixes

--- frontend/src/components/CreatePin.jsx
+++ frontend/src/components/CreatePin.jsx
@@ -31,7 +31,37 @@ function  CreatePin(){
         } else {
             setWrongImageType(true)
         }
-    }
+    };
+    const savePin = () => {
+        if (title && about && destination && imageAsset?._id && category) {
+            const doc = {
+                _type: 'pin',
+                title,
+                about,
+                destination,
+                image: {
+                    _type: 'image',
+                    asset: {
+                        _type: 'reference',
+                        _ref: imageAsset?._id,
+                    },
+                },
+                category,
+            };
+            client.create(doc).then(() => {
+                navigate('/');
+            });
+        } else {
+            setFields(true);
+
+            setTimeout(
+                () => {
+                    setFields(false);
+                },
+                2000,
+            );
+        }
+    };
     return(
         <div className="flex flex-col justify-center items-center mt-5 lg: h-4/5">
             {fields && (
@@ -114,6 +144,15 @@ function  CreatePin(){
                                     </option>
                                 ))}
                             </select>
+                        </div>
+                        <div className="flex justify-end items-end mt-5">
+                            <button
+                                type="button"
+                                onClick={savePin}
+                                className="bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none"
+                            >
+                                Save Pin
+                            </button>
                         </div>
                     </div>
                 </div>
