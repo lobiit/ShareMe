a22b452ae679d77aa16a850f6d98f03c2dc1ae16
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-04 18:50:39 +0300

    clean up

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,17 +3,12 @@ import logo from '../assets/logo.png';
 import {Link, NavLink} from "react-router-dom";
 import {RiHomeFill} from "react-icons/ri";
 import {categories} from "../utils/data";
+
+
 const isNotActiveStyle = "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
 const isActiveStyle = "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
 
-// const categories = [
-//     {name: 'Animals'},
-//     {name: 'Wallpapers'},
-//     {name: 'Photography'},
-//     {name: 'Gaming'},
-//     {name: 'Coding'},
-//     {name: 'Other'}
-//     ]
+
 const SideBar= ({closeToggle})=>{
     const handleCloseSideBar = () => {
         if(closeToggle) closeToggle(false);
