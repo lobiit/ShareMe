6f903f69b606ce499a7d9c50ae6cbdaa5cf88d3d
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-04 18:48:50 +0300

    adding categories to the side bar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import logo from '../assets/logo.png';
 import {Link, NavLink} from "react-router-dom";
 import {RiHomeFill} from "react-icons/ri";
-
+import {categories} from "../utils/data";
 const isNotActiveStyle = "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
 const isActiveStyle = "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
 
-const categories = [
-    {name: 'Animals'},
-    {name: 'Wallpapers'},
-    {name: 'Photography'},
-    {name: 'Gaming'},
-    {name: 'Coding'},
-    {name: 'Other'}
-    ]
+// const categories = [
+//     {name: 'Animals'},
+//     {name: 'Wallpapers'},
+//     {name: 'Photography'},
+//     {name: 'Gaming'},
+//     {name: 'Coding'},
+//     {name: 'Other'}
+//     ]
 const SideBar= ({closeToggle})=>{
     const handleCloseSideBar = () => {
         if(closeToggle) closeToggle(false);
@@ -33,11 +33,14 @@ const SideBar= ({closeToggle})=>{
                     <h3 className="mt-3 px-5 text-base 2xl:text-xl">
                         Discover categories
                     </h3>
-                    {categories.slice(0, categories.length - 1).map((category)=>(
-                        <NavLink to={`/category/${category.name}`}
-                                 className={({isActive})=> isActive? isActiveStyle: isNotActiveStyle}
-                                 onClick={handleCloseSideBar}
-                        key={category.name}>
+                    {categories.slice(0, categories.length - 1).map((category) => (
+                        <NavLink
+                            to={`/category/${category.name}`}
+                            className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+                            onClick={handleCloseSideBar}
+                            key={category.name}
+                        >
+                            <img src={category.image} className="w-8 h-8 rounded-full shadow-sm" />
                             {category.name}
                         </NavLink>
                     ))}
